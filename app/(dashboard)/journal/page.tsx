845875ededf1f920e8c25f14af8a4f0fdf023218
Brad <brad@brads-mbp.lan>
2024-04-04 14:04:17 -0700

    init commit, scaffold

---
+++ app/(dashboard)/journal/page.tsx
@@ -0,0 +1,33 @@
+import NewEntryCard from "@/components/NewEntryCard";
+import EntryCard from "@/components/EntryCard";
+import { getUserByClerkId } from "@/utils/auth"
+import { prisma } from "@/utils/db"
+
+const getEntries = async () => {
+    const user = await getUserByClerkId();
+    const entries = await prisma.journalEntry.findMany({
+        where: {
+            userId: user.id
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+    return entries
+}
+
+const JournalPage = async () => {
+    const entries = await getEntries()
+    
+    return(
+        <div className="p-10 bg-zinc-400/10 h-full">
+            <h2 className="text-3xl mb-8">Journal</h2>
+            <div className="grid grid-cols-3 gap-4">
+                <NewEntryCard />
+                {entries.map(entry => <EntryCard key={entry.id} entry={entry} />)}
+            </div>
+        </div>
+    )
+}
+
+export default JournalPage
