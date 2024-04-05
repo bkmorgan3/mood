cd755aab7f3e53aa12f5e6a6adc36d449498a4c0
Brad <brad@brads-mbp.lan>
2024-04-04 21:30:58 -0700

    edit entry

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,7 +1,29 @@
 'use client'
+import { updateEntry } from "@/utils/api"
+import { useState } from "react"
+import { useAutosave } from "react-autosave"
 
 const Editor = ({entry}) => {
-    return <div>{entry.content}</div>
+    const [value, setValue] = useState(entry.content)
+    const [isLoading, setIsLoading] = useState(false)
+    useAutosave({
+        data: value,
+        onSave: async (_value) => {
+            setIsLoading(true)
+            const updated = await updateEntry(entry.id, _value)
+            setIsLoading(false)
+        }
+    })
+    return (
+        <div className="h-full w-full">
+            {isLoading && <div>Loading...</div>}
+            <textarea
+            value={value}
+            onChange={(e) => setValue(e.target.value)} 
+            className="h-full w-full"
+        />
+        </div>
+    )
 }
 
 export default Editor
